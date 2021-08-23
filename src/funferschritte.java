00762e90d1375b02f2e1ae2771c7e26a2716f6bf
Akif Aydin <akifnidya@hotmail.de>
2021-08-23 18:02:31 +0200

    V3 18:00

---
+++ src/funferschritte.java
@@ -0,0 +1,20 @@
+public class funferschritte {
+
+    public static void ueb1() {
+        for (int i = 1; i < 100; i++) {
+            if (i % 5 != 0)
+                continue;
+            System.out.print(i + ",");
+        }
+    }
+
+    public static void ueb2(int wert) {
+        if(wert % wert == 0)
+    }
+
+
+    public static void main(String[] args) {
+        ueb1();
+
+    }
+}
