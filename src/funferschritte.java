77d395f4ac2e4756a666303e244106b6af0202b5
Akif Aydin <akifnidya@hotmail.de>
2021-08-23 21:32:45 +0200

    V3 21:00

--- src/funferschritte.java
+++ src/funferschritte.java
@@ -6,15 +6,23 @@ public class funferschritte {
                 continue;
             System.out.print(i + ",");
         }
+        System.out.println();
     }
 
     public static void ueb2(int wert) {
-        if(wert % wert == 0)
+        for (long i = 2; i * i <= wert; i++) {
+            if (wert % i == 0) {
+                System.out.println("kein Prim");;
+            }
+        }
+        System.out.println(wert);;
     }
 
 
     public static void main(String[] args) {
+        System.out.println("5er Zahlen Reihe");
         ueb1();
-
+        System.out.println("Primzahlen");
+        ueb2(4);
     }
 }
